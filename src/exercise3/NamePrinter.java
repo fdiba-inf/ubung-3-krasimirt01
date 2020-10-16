62309996fe9e93d17a4a33d9acdf9990a36f4dce
krasimirt01 <krasimir.tashkov@fdiba.tu-sofia.bg>
2020-10-16 06:06:17 +0000

    -

--- src/exercise3/NamePrinter.java
+++ src/exercise3/NamePrinter.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class NamePrinter {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String name = input.next();
-        for(int i=0;i<name.length();i++){
-          char letter = name.charAt(i);
-          System.out.println("*"+letter+"*");
-        }
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    String name = input.next();
+    for (int i = 0; i < name.length(); i++) {
+      char letter = name.charAt(i);
+      System.out.println("*" + letter + "*");
     }
+  }
 }
--- src/exercise3/PrimeNumber.java
+++ src/exercise3/PrimeNumber.java
@@ -4,23 +4,21 @@ import java.util.Scanner;
 
 public class PrimeNumber {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
 
-        int n = input.nextInt();
-        int d = 2;
-        boolean prime = true;
-        while(d<n){
-          if (n%d==0){
-            prime = false;
-            break;
+    int n = input.nextInt();
+    int d = 2;
+    boolean prime = true;
+    while (d < n) {
+      if (n % d == 0) {
+        prime = false;
+        break;
 
-          }
-          d++;
+      }
+      d++;
 
-        }
-        System.out.println("Prime number: "+prime);
-    } 
-}       
-
-      
+    }
+    System.out.println("Prime number: " + prime);
+  }
+}
